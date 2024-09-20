c85cbe769c58875502ae1fa5b77fbbfb3b883c7a
Lula <lulacode@gmail.com>
2024-09-20 16:11:16 -0300

    Pequenos refactorings pra ficar + didático

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -8,13 +8,20 @@ final class UserTest extends TestCase
 {
     public function test_greeting()
     {
+        //arrange
         $dbMock = $this->getMockBuilder(Database::class)
             ->onlyMethods(['getPersonByID'])
             ->getMock();
         $mockPerson = new \stdClass();
         $mockPerson->name = 'Bob';
+        $mockPerson->id = 2;
         $dbMock->method('getPersonByID')->willReturn($mockPerson);
         $test = new User($dbMock);
-        $this->assertEquals('Hello Bob', $test->greeting(2));
+
+        // act
+        $actual_result = $test->greeting(2);
+        
+        //assert
+        $this->assertEquals('Hello Bob', $actual_result);
     }
 }
